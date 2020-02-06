04dbef1c124ea0812f1fe0313282a64190502be7
Jodi N Kansagor <joedvice@gmail.com>
2020-02-06 14:44:49 -0800

    artist Item page

--- src/components/artist/Form.js
+++ src/components/artist/Form.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styles from './Form.ss'
 
-const ArtistInput = ({ artistName, setArtist}) => (
-  <section>
+const ArtistInput = ({ artistName, setArtist, onSubmit }) => (
+  <form className={styles.form}>
     <input type="text" value={artistName} onChange={({ target }) => setArtist(target.value)} />
-  </section>
+    <button onSubmit={onSubmit}>Seach!</button>
+  </form>
 );
 
 ArtistInput.propTypes = {
   artistName: PropTypes.string,
-  setArtist: PropTypes.func.isRequired
+  setArtist: PropTypes.func.isRequired,
+  onSubmit: PropTypes.func.isRequired
 };
 
 export default ArtistInput;
