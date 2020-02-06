36e7277f9b0c337d78c194217eea2aa339524e3b
Jodi N Kansagor <joedvice@gmail.com>
2020-02-06 15:40:30 -0800

    wrote artist fetch call

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import ArtistContainer from '../containers/ArtistContainer';
+import Header from './common/Header';
 
 export default function App() {
-  return <ArtistContainer />;
+  return (
+    <>
+      <Header />
+      <ArtistContainer />
+    </>
+  );
 }
-  
